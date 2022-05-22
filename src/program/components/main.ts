4b63952efd013fb46e0660ae3834c52cd2a89487
rerender2021 <rerender2021@outlook.com>
2022-05-22 17:38:14 +0800

    add recipe

--- src/program/components/main.ts
+++ src/program/components/main.ts
@@ -1,13 +1,16 @@
 import { Area, createGridLayout, GridLayout } from "../../components";
+import { RecipeAddFrameNumber } from "./recipes";
 import { Sidebar } from "./sidebar";
 
 export class Main extends Area {
 	private sidebar: Sidebar;
+	private recipe: Area;
 
 	protected onCreate(): GridLayout {
 		const { window } = this;
 
 		this.sidebar = new Sidebar(window).create();
+		this.recipe = new RecipeAddFrameNumber(window).create();
 
 		const container = this.onCreateLayout();
 		return container;
@@ -21,12 +24,13 @@ export class Main extends Area {
 			columns: "200dpx 1",
 			areas: {
 				sidebar: { x: 0, y: 0 },
-				content: { x: 1, y: 0 },
+				recipe: { x: 1, y: 0 },
 			},
 		};
 
 		const container = createGridLayout(window, containerLayout);
 		container.addControl(this.sidebar.control, container.areas.sidebar);
+		container.addControl(this.recipe.control, container.areas.recipe);
 
 		return container;
 	}
---
+++ src/program/components/recipes/template.ts
@@ -0,0 +1,28 @@
+import { Button } from "ave-ui";
+import { Area, createGridLayout, GridLayout } from "../../../components";
+
+export class RecipeTemplate extends Area {
+	protected onCreate(): GridLayout {
+		const container = this.onCreateLayout();
+		return container;
+	}
+
+	private onCreateLayout() {
+		const { window } = this;
+
+		const containerLayout = {
+			rows: "1",
+			columns: "1",
+			areas: {
+				content: { x: 0, y: 0 },
+			},
+		};
+
+		const button = new Button(window);
+		button.SetText("Button");
+
+		const container = createGridLayout(window, containerLayout);
+		container.addControl(button, containerLayout.areas.content);
+		return container;
+	}
+}
