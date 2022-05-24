d2a9d85be4cbe84c0e003d93d69603f00cda941e
rerender2021 <rerender2021@outlook.com>
2022-05-24 23:41:18 +0800

    add run button

--- src/program/components/recipes/add-frame-number.ts
+++ src/program/components/recipes/add-frame-number.ts
@@ -11,6 +11,10 @@ export class RecipeAddFrameNumber extends Area {
 	private step1: Label;
 	private dropArea: DropArea;
 
+	//
+	private step2: Label;
+	private run: Button;
+
 	protected onCreate(): GridLayout {
 		const { window } = this;
 		const codec = state.getApp().GetImageCodec();
@@ -31,6 +35,13 @@ export class RecipeAddFrameNumber extends Area {
 		const uploadIcon = codec.Open(ResourceSource.FromPackedFile(assetPath("components/upload-128.png"))).Resize(100, 100, ImageFilterType.Linear);
 		this.dropArea = new DropArea(window, uploadIcon);
 
+		this.step2 = new Label(window);
+		this.step2.SetText("Step1: Click Run");
+		this.step2.SetAlignHorz(AlignType.Near);
+
+		this.run = new Button(window);
+		this.run.SetText("Run");
+
 		const container = this.onCreateLayout();
 		return container;
 	}
@@ -39,24 +50,40 @@ export class RecipeAddFrameNumber extends Area {
 		const { window } = this;
 
 		const containerLayout = {
-			rows: "50dpx 30dpx 200dpx 30dpx 15dpx 150dpx 1",
+			rows: "50dpx 30dpx 200dpx 30dpx 15dpx 150dpx 15dpx 30dpx 15dpx 30dpx 1",
 			columns: "1 1 1",
 			areas: {
 				description: { x: 1, y: 1 },
 				demoImage: { x: 1, y: 2 },
+				//
 				step1: { x: 1, y: 3 },
 				dropArea: { x: 1, y: 5 },
+
+				//
+				step2: { x: 1, y: 7 },
+				run: { x: 1, y: 9 },
 			},
 		};
-
-		const button = new Button(window);
-		button.SetText("Button");
 
 		const container = createGridLayout(window, containerLayout);
 		container.addControl(this.demoImage, containerLayout.areas.demoImage);
 		container.addControl(this.description, containerLayout.areas.description);
 		container.addControl(this.step1, containerLayout.areas.step1);
 		container.addControl(this.dropArea.control, containerLayout.areas.dropArea);
+		container.addControl(this.step2, containerLayout.areas.step2);
+
+		const runLayout = {
+			rows: "1",
+			columns: "1 150dpx 1",
+			areas: {
+				content: { x: 1, y: 0 },
+			},
+		};
+		const runArea = createGridLayout(window, runLayout);
+		runArea.addControl(this.run, runLayout.areas.content);
+
+		container.addControl(runArea.control, containerLayout.areas.run);
+
 		return container;
 	}
 }
